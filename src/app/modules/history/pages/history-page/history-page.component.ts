0e1aee38e65730d71a31b09899fd5364c3159391
Armando Pi <armandopinedag@outlook.com>
2024-07-10 21:19:50 -0700

    add new components

---
+++ src/app/modules/history/pages/history-page/history-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-history-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './history-page.component.html',
+  styleUrl: './history-page.component.css'
+})
+export class HistoryPageComponent {
+
+}
