dfdf9214b0d0b0c5a8d9b8889f8031ebf3d5e49b
Armando Pi <armandopinedag@outlook.com>
2024-09-09 16:16:04 -0600

    create favorites component to render shared modules

--- src/app/modules/favorites/favorites-routing.module.ts
+++ src/app/modules/favorites/favorites-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FavoritesPageComponent } from './pages/favorites-page/favorites-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: FavoritesPageComponent,
+    outlet: 'child'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
+++ src/app/modules/favorites/pages/favorites-page/favorites-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { PlayListBodyComponent } from "../../../../shared/components/play-list-body/play-list-body.component";
+import { PlayListHeaderComponent } from '@shared/components/play-list-header/play-list-header.component';
 
 @Component({
   selector: 'app-favorites-page',
   standalone: true,
-  imports: [],
+  imports: [
+    PlayListHeaderComponent,
+    PlayListBodyComponent,
+  ],
   templateUrl: './favorites-page.component.html',
   styleUrl: './favorites-page.component.css'
 })
