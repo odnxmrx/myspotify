5e41d5001cac521b35fedba6ff4115c31ac3ac18
ArmandoG <armandopinedag@outlook.com>
2024-10-03 15:48:55 -0600

    add minor changes to auth-routing

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //para ir a /auth/login:
+    path: 'login', //sería localhost:4200/auth/login
+    component: AuthPageComponent,
+  },
+  {
+    path: '', //será localhost:4200/auth
+    component: AuthPageComponent,
+  },
+  {
+    path: '**', //Error 404 Not Found
+    redirectTo: '/auth/login',
+    // localhost:4200/auth/gfgvdrfs
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AuthRoutingModule { }
+export class AuthRoutingModule {}
