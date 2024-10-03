1da872c44f9a2ca5447a9ca9cbd49d7ed127d95d
ArmandoG <armandopinedag@outlook.com>
2024-10-03 17:42:47 -0600

    create reactive login form with validators

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-
+import { ReactiveFormsModule } from '@angular/forms'; //No util aca
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
-export class AuthModule { }
+export class AuthModule {}
--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-auth-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './auth-page.component.html',
-  styleUrl: './auth-page.component.css'
+  styleUrl: './auth-page.component.css',
 })
-export class AuthPageComponent {
+export class AuthPageComponent implements OnInit {
+  formLogin: FormGroup = new FormGroup({});
 
+  ngOnInit(): void {
+    this.formLogin = new FormGroup({
+      //Obj de nombres a observar
+      email: new FormControl(
+        '', // 1er param: Estado-> Valor inicializado,
+        [Validators.required, Validators.email]
+      ), //2do param: Array de validaciones
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(18),
+      ]),
+    });
+  }
+  sendLogin(): void {
+    const body = this.formLogin?.value; //toma lo que está en el form
+    // console.log('BODYYYYYY => ', body);
+  }
 }
