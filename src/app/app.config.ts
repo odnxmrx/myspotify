26626a9f79077c463282aff2a9ef040772ee025d
ArmandoG <armandopinedag@outlook.com>
2024-11-21 22:52:59 -0600

    inject interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,6 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(),
-    provideHttpClient(withInterceptors([injectSessionInterceptor])),
+    provideHttpClient(withInterceptors([injectSessionInterceptor])), //Interceptor project-wide
   ],
 };
