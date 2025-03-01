2ea5c2cc15df1c338fd4cc58e60c041105d12039
odnxmrx <armandopinedag@outlook.com>
2025-03-01 00:03:41 -0600

    add pipe async to improve performance

--- src/app/modules/history/pages/history-page/history-page.component.ts
+++ src/app/modules/history/pages/history-page/history-page.component.ts
@@ -1,29 +1,28 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { TrackModel } from '@core/models/track.model';
 import { SearchComponent } from '@modules/history/components/search/search.component';
 import { SearchService } from '@modules/history/services/search.service';
 import { PlayListBodyComponent } from '@shared/components/play-list-body/play-list-body.component';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-history-page',
   standalone: true,
-  imports: [PlayListBodyComponent, SearchComponent],
+  imports: [PlayListBodyComponent, SearchComponent, AsyncPipe],
   templateUrl: './history-page.component.html',
   styleUrl: './history-page.component.css',
 })
 export class HistoryPageComponent {
-  // Creamos array de lista de resultados de tipo TrackModel
-  listResults: TrackModel[] = [];
+  // Creamos array de lista de resultados ($ observable) de tipo TrackModel
+  listResults$: Observable<any> = of([]);
 
   // inyectamos servicio en constructor
   constructor(private searchService: SearchService) {}
 
   // funcion que manejará lo que recibe del hijo
   receiveData(event: string): void {
-    // console.log('dato obtenido de componente hijo: ', event);
-    this.searchService.searchTracks$(event).subscribe((res) => {
-      // console.log('suscccccc servvvv la res: ', res);
-      this.listResults = res.data;
-    });
+    // con esto, ya está escuchando en automático
+    this.listResults$ = this.searchService.searchTracks$(event);
   }
 }
--- src/app/modules/history/services/search.service.ts
+++ src/app/modules/history/services/search.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +15,8 @@ export class SearchService {
 
   // $ - indica que retornara observable
   searchTracks$(term: string): Observable<any> {
-    return this.http.get(`${this.URI}/tracks?src=${term}`);
+    return this.http
+      .get(`${this.URI}/tracks?src=${term}`)
+      .pipe(map((resp: any) => resp.data)); // aplicamos pipe para filtrar el arreglo de 'data'
   }
 }
