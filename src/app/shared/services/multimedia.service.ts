56643a8661ccdc3f113fd6f39f619f5e5901175f
odnxmrx <armandopinedag@outlook.com>
2025-03-17 19:01:36 -0600

    set audio element from function

--- src/app/shared/services/multimedia.service.ts
+++ src/app/shared/services/multimedia.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/track.model';
 import { BehaviorSubject, Observable, Observer, Subject } from 'rxjs';
 
 @Injectable({
@@ -18,5 +19,19 @@ export class MultimediaService {
   // variable 'trackInfo' de tipo BehaviorSubject incializada como 'undefined'
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined);
 
-  constructor() {}
+  constructor() {
+    this.audio = new Audio(); // inicializamos 'audio' de la clase 'Audio'
+    this.trackInfo$.subscribe((resOk) => {
+      if (resOk) {
+        this.setAudio(resOk);
+      }
+    });
+  }
+
+  // Funcion publica
+  public setAudio(track: TrackModel): void {
+    // console.log('recibí evento', track);
+    this.audio.src = track.url; // asignamos a propiedad 'src' de variable 'audio', la 'url'
+    this.audio.play();
+  }
 }
